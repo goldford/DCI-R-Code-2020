0a681e0272640cc4f4b6211cc374510af5ba1a52
aarkilanian <a.arkilanian@gmail.com>
2021-09-24 10:51:14 -0400

    Changes function object naming to reflect end terminology

--- 2021 Debug/Alex modifications/Functions/find_ends.R
+++ 2021 Debug/Alex modifications/Functions/find_ends.R
@@ -5,10 +5,10 @@ get_dowstream_end <- function(seg.membership, network){
   member.segs <- network %N>% filter(membership == seg.membership) %>% data.frame()
   
   # Get downstream entrance node
-  entrance <- member.segs[which.min(member.segs$label),]$label
+  d.end <- member.segs[which.min(member.segs$label),]$label
   
   # Return entrance node label
-  return(entrance)
+  return(d.end)
   
 }
 
@@ -19,13 +19,13 @@ get_upstream_ends <- function(seg.membership, network){
   member.segs <- network %N>% filter(membership == seg.membership) %>% data.frame()
   
   # Get upstream exit nodes
-  exits <- member.segs %>%
+  u.ends <- member.segs %>%
     filter(type %in% "Barrier") %>%
     pull(label)
   
   # Return exit nodes labels
-  if(length(exits) == 0){
+  if(length(u.ends) == 0){
     return(NA)
-  } else {return(exits)}
+  } else {return(u.ends)}
   
 }
