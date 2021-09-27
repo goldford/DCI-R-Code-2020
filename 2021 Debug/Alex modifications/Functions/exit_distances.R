2dd2771e4a096846b1c204a488bf595c8c0ff68a
aarkilanian <a.arkilanian@gmail.com>
2021-09-26 21:27:30 -0400

    Created sub-function to get exit distance

--- 2021 Debug/Alex modifications/Functions/exit_distances.R
+++ 2021 Debug/Alex modifications/Functions/exit_distances.R
@@ -1,7 +1,12 @@
 get_exit_distances <- function(network = NULL, seg.edges = NULL, seg.dist.perm = NULL){
   
-  # extract nodes
-  network %>%
-    activate(nodes)
+apply(network %>% activate(nodes) %>% data.frame(), 1, FUN = node_to_exit, seg.edges)
+  
+}
+
+node_to_exit <- function(node, seg.edges){
+  
+  # Gather segment edge nodes for given node
+  cur.edges <- 
   
 }
